cf7dd39986f03123cde3e8df80d71f89825160df
Ibrahim Traore <ibrahimmahamadoutraore122@gmail.com>
2026-03-26 23:00:27 +0100

    Debugage au niveau des relationship

--- Modification/app.py
+++ Modification/app.py
@@ -1,24 +1,18 @@
 ## 1. Le moteur (Engine) Coeur de la connexion 
 from sqlalchemy import create_engine 
 import pandas as pd 
+import os
 
-engine=create_engine(r'sqlite:///C:/Users/pc/Desktop/Projet/FLASK_INTRO/Architect_flask/modification/produit.db')
+basedir = os.path.abspath(os.path.dirname(__file__))
+print(os.path.join(basedir, 'database.db'))
+engine=create_engine(r'sqlite:///' + os.path.join(basedir, 'database.db'))
 
 with engine.connect() as connection:
     ## Requete simple 
-    produit=pd.read_sql_query("SELECT * FROM produits",connection)
+    produit=pd.read_sql_query("SELECT * FROM product",connection)
     print(produit)
     print("C'est tout bon !")
 
-    # 2ème methode 
-
-    produit2=pd.read_sql_table(
-        "produits",
-        connection,
-        columns=["id_produit","nom_produit","description"]
-                              )
-    print(produit2)
-    print("Affiche pour la deuxieme methode")
 
 
 
