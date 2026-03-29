a2e2af619fc28cac040a59bfead2a9ad6338d442
Ibrahim Traore <ibrahimmahamadoutraore122@gmail.com>
2026-03-29 10:15:45 +0200

    Problème resolue

--- Modification/app.py
+++ Modification/app.py
@@ -9,7 +9,10 @@ engine=create_engine(r'sqlite:///' + os.path.join(basedir, 'database.db'))
 
 with engine.connect() as connection:
     ## Requete simple 
-    produit=pd.read_sql_query("SELECT * FROM product",connection)
+    produit =pd.read_sql_query("SELECT * FROM product ", connection)
+    categorie = pd.read_sql_query("SELECT * FROM categories", connection)
+    print(categorie)
+    print("Pour les categorie c'est ok !")
     print(produit)
     print("C'est tout bon !")
 
